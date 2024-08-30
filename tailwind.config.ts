fb3d14b13899c32effc81d14466e797e8502e244
SCAREcrow0367 <kylermoore65@gmail.com>
2024-08-30 14:08:48 -0600

    Got fonts, colors,  and imports in proper locations. Now going to build cards for google and meta. With a reviewer comp.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,21 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        gold: "#F5C044",
+        green: "#4CA154",
+        offgray: "#D4D4D8",
+        eggshell: "#FAFAF9",
+        white: "#FFFFFF",
+        black: "#18181B",
+        darkgray: "#52525A",
+      },
+      fontSize: {
+        sm: "14px",
+        md: "16px",
+        lg: "20px",
+        xl: "36px",
+      },
     },
   },
   plugins: [],
