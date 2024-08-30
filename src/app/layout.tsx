731a6260a507fd14a58daf49cccb2a6aea122767
SCAREcrow0367 <kylermoore65@gmail.com>
2024-08-30 13:54:38 -0600

    updated punycode deprecation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import punycode from 'punycode';
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,6 +16,8 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const punycode = require('punycode');
+
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
